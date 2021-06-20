33312c9ff300a960a7a8406ebe6f72d050a39626
Domingo Ramirez <domingo.ramirez.artigas@gmail.com>
2021-06-20 14:37:11 -0500

    Initial commit

---
+++ src/handlers/put-item.js
@@ -0,0 +1,33 @@
+const dynamodb = require('aws-sdk/clients/dynamodb');
+const docClient = new dynamodb.DocumentClient();
+
+const tableName = process.env.TABLE;
+
+exports.putItemHandler = async (event) => {
+    if (event.httpMethod !== 'POST') {
+        throw new Error(`postMethod only accepts POST method, you tried: ${event.httpMethod} method.`);
+    }
+    console.info('received:', event);
+
+    const body = JSON.parse(event.body)
+    
+    const id = body.id;
+    
+    const name = body.name;
+
+    var params = {
+        TableName : tableName,
+        Item: { id : id, name: name }
+    };
+
+    const result = await docClient.put(params).promise();
+
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify(body)
+    };
+
+    console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`);
+    
+    return response;
+}
